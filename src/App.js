b01e91e4bd733c8fc906bf54f55ea3866892f2c9
tejesh <tejesh.diviti0416@gmail.com>
2022-08-08 17:21:43 +0530

    add few changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>This is heading 2</h1>
       <h1>This is heading</h1>
       <h1>Name:VENKATESH</h1>
-      <h1 className="css">Name:DIVITI TEJESH</h1>
+      <h1>Name:DIVITI TEJESH</h1>
       <h1>NAME:I.REVANTH</h1>
       <h1>MY NAME IS : HEMANTH KUMAR</h1>
     </div>
